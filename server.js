c3c0a4ff267aafcd86ba3dd542f1f1ef82667646
Dat Hoang <dathoangnd@gmail.com>
2019-10-17 17:09:00 +0700

    d

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 
-  
 }).listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
